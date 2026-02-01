db80a14c752ec496c7e84ecd064af99b3215901d
zmm2025 <zmm2025@gmail.com>
2026-02-01 12:48:39 -0500

    Export new build

--- docs/fish-out-of-water.service.worker.js
+++ docs/fish-out-of-water.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1769965620|2199565274';
+const CACHE_VERSION = '1769968093|4672768711';
 /** @type {string} */
 const CACHE_PREFIX = 'Fish Out of Wate-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;

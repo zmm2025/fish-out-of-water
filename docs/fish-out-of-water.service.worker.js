1c3b6ebf4d7f4529d3870269d8526ad95d21d6e0
zmm2025 <zmm2025@gmail.com>
2026-02-03 10:01:56 -0500

    Show title screen, fix floating black pixels, fix debugger errors, update Web and Windows exports

--- docs/fish-out-of-water.service.worker.js
+++ docs/fish-out-of-water.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1769983157|3125214247';
+const CACHE_VERSION = '1770130644|3522125694';
 /** @type {string} */
 const CACHE_PREFIX = 'Fish Out of Wate-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
